6625d1378bf2e756d61dd992fa8ade513f3c5168
msung99 <msung6924@naver.com>
2022-11-12 22:14:50 +0900

    feat: GetAllMusic_in_folder

--- src/main/java/maestrogroup/core/music/model/Music.java
+++ src/main/java/maestrogroup/core/music/model/Music.java
@@ -1,4 +1,18 @@
 package maestrogroup.core.music.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Music {
+    int musicIdx;
+    int BPM;
+    int folderIdx;
+    String musicImgUrl;
+    String musicName;
+
+
 }
