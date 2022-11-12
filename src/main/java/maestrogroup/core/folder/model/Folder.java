17b24c99cdce19cf48702c00946d66cf68bda7b0
msung99 <msung6924@naver.com>
2022-11-12 15:40:34 +0900

    feat: GetAllFolder

--- src/main/java/maestrogroup/core/folder/model/Folder.java
+++ src/main/java/maestrogroup/core/folder/model/Folder.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.folder.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 // folderIdx, folderImgUrl, folderName, teamIdx
@@ -10,4 +11,11 @@ public class Folder {
     private String folderImgUrl;
     private String folderName;
     private int teamIdx;
+
+    public Folder(int folderIdx, String folderImgUrl, String folderName, int teamIdx) {
+        this.folderIdx = folderIdx;
+        this.folderImgUrl = folderImgUrl;
+        this.folderName = folderName;
+        this.teamIdx = teamIdx;
+    }
 }
