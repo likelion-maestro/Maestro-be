9a71537cc2988195ec3496992308639f59d62f7a
msung99 <msung6924@naver.com>
2022-11-12 12:55:53 +0900

    feat: Patch Team

---
+++ src/main/java/maestrogroup/core/team/model/PatchTeamReq.java
@@ -0,0 +1,12 @@
+package maestrogroup.core.team.model;
+
+import lombok.Data;
+
+@Data
+public class PatchUserReq {
+    private int teamIdx;
+    private String teamName;
+    private String teamImgUrl;
+
+    
+}
