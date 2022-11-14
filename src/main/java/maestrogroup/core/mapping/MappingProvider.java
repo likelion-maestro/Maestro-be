a872ff57321b0e0575e6044ba5448de16516b881
msung99 <msung6924@naver.com>
2022-11-14 15:56:46 +0900

    feat: getTeamMembers - Using ManyToMany Relation

--- src/main/java/maestrogroup/core/mapping/MappingProvider.java
+++ src/main/java/maestrogroup/core/mapping/MappingProvider.java
@@ -1,7 +1,23 @@
 package maestrogroup.core.mapping;
 
+import maestrogroup.core.mapping.model.Mapping;
+import maestrogroup.core.user.model.GetUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MappingProvider {
+
+    @Autowired
+    private final MappingDao mappingDao;
+
+    public MappingProvider(MappingDao mappingDao){
+        this.mappingDao = mappingDao;
+    }
+
+    public List<GetUser> getTeamMembers(int teamIdx){
+        return mappingDao.getTeamMembers(teamIdx);
+    }
 }
