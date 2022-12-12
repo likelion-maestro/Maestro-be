bf9d462861cf627761c4d300cb3e4e90f0a01b17
msung99 <msung6924@naver.com>
2022-12-12 16:09:31 +0900

    feat: validateRefreshToken (RefreshToken 유효성 검ê성 검사)

---
+++ src/main/java/maestrogroup/core/Security/RefreshToken.java
@@ -0,0 +1,12 @@
+package maestrogroup.core.Security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken{
+    String refreshToken;
+}
