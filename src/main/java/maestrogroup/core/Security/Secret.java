817a8d3ec1adadd3795aa46693a616ab91e622de
msung99 <msung6924@naver.com>
2022-12-12 15:18:17 +0900

    refactor: Seperate Access_Refresh Token

--- src/main/java/maestrogroup/core/Security/Secret.java
+++ src/main/java/maestrogroup/core/Security/Secret.java
@@ -2,7 +2,8 @@ package maestrogroup.core.Security;
 
 // .gitignore에 추가하는거 앚지 말자! 외부에 절대 유출되서는 안되는 비밀키(Secret key)이다.
 public class Secret {
-    public static String JWT_SECRET_KEY = "nicetomeetyousfhyfesihfshiufeshuifsehuifsejiufsehisfehhfsifhsuiuhfsihfuisfsihueihufsehiusfehiufsehuifseihusfhiusefhiu";
+    public static String ACCESS_TOKEN_SECRET_KEY = "nicetomeetyousfhyfesihfshiufeshuifsehuifsejiufsehisfehhfsifhsuiuhfsihfuisfsihueihufsehiusfehiufsehuifseihusfhiusefhiu";
+    public static String REFRESH_TOKEN_SECRET_KEY = "inqweniqwoeqnioweioneoinroinrnoinodnoringondoiringoincoinrotihbnoithneorifkkwioekoweirkoiwkierokwioeroiwerio";
     public static String USER_INFO_PASSWORD_KEY = "o9pqYVC9-F8_.PEzEiw!L9F6.AYj9jcfVJ*_i.ifXYnyE68kix@Q2dL6rw*bV-rpdZYwcqZG-jPF-fw3CiJyKsfZ778ks-*jnZn";
 }
 
