9041ccd17bb4e554faf9624a7d5ff89aa43d037b
jeongseungchan <jschan0911@naver.com>
2022-12-29 04:43:02 +0900

    feat: Music 모델 생성 기능 추가

--- src/main/java/maestrogroup/core/music/MusicDao.java
+++ src/main/java/maestrogroup/core/music/MusicDao.java
@@ -1,11 +1,13 @@
 package maestrogroup.core.music;
 
 import maestrogroup.core.music.model.Music;
+import maestrogroup.core.music.model.PostMusicReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.Array;
 import java.util.List;
 
 @Repository
@@ -23,12 +25,17 @@ public class MusicDao {
         return this.jdbcTemplate.query(GetAllMusicQuery,
                 (rs, rowNum) -> new Music(
                  rs.getInt("musicIdx"),
-                 rs.getInt("BPM"),
+                 rs.getInt("bpm"),
                  rs.getInt("folderIdx"),
                  rs.getString("musicImgUrl"),
                         rs.getString("musicName"),
                         rs.getInt("circleNum"),
                         rs.getInt("totalNum")),
                 folderIdx);
+    }
+    public void createMusic(PostMusicReq postMusicReq, int folderIdx) {
+        String createMusicQuery = "insert into Music (musicName, bpm, circleNum, totalNum, musicImgUrl, folderIdx) VALUES (?, ?, ?, ?, ?, ?)";
+        Object[] createMusicParams = new Object[]{postMusicReq.getMusicName(), postMusicReq.getBpm(), postMusicReq.getCircleNum(), (double)60 / postMusicReq.getBpm() * postMusicReq.getCircleNum(), postMusicReq.getMusicImgUrl(), folderIdx};
+        this.jdbcTemplate.update(createMusicQuery, createMusicParams);
     }
 }
--- src/main/java/maestrogroup/core/music/MusicService.java
+++ src/main/java/maestrogroup/core/music/MusicService.java
@@ -1,7 +1,19 @@
 package maestrogroup.core.music;
 
+import maestrogroup.core.music.model.PostMusicReq;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MusicService {
+    @Autowired
+    private final MusicDao musicDao;
+
+    public MusicService(MusicDao musicDao){
+        this.musicDao = musicDao;
+    }
+
+    public void createMusic(PostMusicReq postMusicReq, int folderIdx) {
+        musicDao.createMusic(postMusicReq, folderIdx);
+    }
 }
