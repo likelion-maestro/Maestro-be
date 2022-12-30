4abd6411629a28f3c01a5c2656de9763004f87ac
msung99 <msung6924@naver.com>
2022-12-30 21:41:11 +0900

    feat: checkDuplicateBlackAcessToken

--- src/main/java/maestrogroup/core/Security/JwtRepository.java
+++ src/main/java/maestrogroup/core/Security/JwtRepository.java
@@ -37,7 +37,22 @@ public class JwtRepository {
                 dbRefreshToken);
     }
 
+    public int checkDuplicateBlackAccessToken(String accessTokenParam){
+        String findBlackUserQuery = "select count(*) from BlackList where accessToken = ?";
+        Object[] accessTokenQueryParams = new Object[]{accessTokenParam};
+        Integer tokenCount = this.jdbcTemplate.queryForObject(findBlackUserQuery, accessTokenQueryParams, Integer.class);
+        System.out.println(tokenCount);
+        return tokenCount;
+    }
+
+    // 만료안된 accessToken을 계속 요청으로 보내서 블랙리스트 db에 과축적 되는 현상을 방지하기 위한 예외처리 요망
+    // => 특정 토큰에 대한 블랙리스트 데이터는 유일성을 보장할 것, 즉 동일한 토큰을 또 보냈을 때 db에 저장되는 현상을 막자.
+    // 단 특정 유저가 짧은 시간에 로그인을 여러번 시도해서 한 유저가 여러개의
+    // 토큰을 지닐 수 있므므로 userIdx값이 아닌, 토큰값을 기준으로 판단하자.
     public void saveBlickList(int userIdx, String accessToken, int exp){
+        int tokenCount = checkDuplicateBlackAccessToken(accessToken);
+        System.out.println(tokenCount);
+        System.out.println("===========================");
         String saveTokenQuery = "insert into BlackList (userIdx, accessToken, expireDate) VALUES (?, ?, ?)";
         Object[] saveTokenQueryParams = new Object[]{userIdx, accessToken, exp};
         this.jdbcTemplate.update(saveTokenQuery, saveTokenQueryParams);
