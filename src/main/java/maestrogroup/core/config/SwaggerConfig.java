67de2872d77f70530b45a69ae3aeaf0bbf08b179
jeongseungchan <jschan0911@naver.com>
2023-01-01 22:11:47 +0900

    feat: swagger 내 jwt 인증 적용 기능 추가

--- src/main/java/maestrogroup/core/config/SwaggerConfig.java
+++ src/main/java/maestrogroup/core/config/SwaggerConfig.java
@@ -6,9 +6,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 // config/SwaggerConfig.java
 @Configuration
@@ -18,6 +24,8 @@ public class SwaggerConfig {  // Swagger
     private static final String API_NAME = "Maestro API";
     private static final String API_VERSION = "0.0.1";
     private static final String API_DESCRIPTION = "Maestro API 명세서";
+    private static final String REFERENCE = "Authorization 헤더 값";
+
 
     @Bean
     public Docket api() {
@@ -25,9 +33,31 @@ public class SwaggerConfig {  // Swagger
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("maestrogroup.core"))  // Swagger를 적용할 클래스의 package명
                 .paths(PathSelectors.any())  // 해당 package 하위에 있는 모든 url에 적용
+                .build()
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(securityScheme()));
+    }
+
+    private SecurityContext securityContext() {
+        return springfox.documentation
+                .spi.service.contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true)
                 .build();
     }
 
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(REFERENCE, authorizationScopes));
+    }
+
+    private ApiKey securityScheme() {
+        String targetHeader = "Authorization";
+        return new ApiKey(REFERENCE, targetHeader, "header");
+    }
 
     public ApiInfo apiInfo() {  // API의 이름, 현재 버전, API에 대한 정보
         return new ApiInfoBuilder()
