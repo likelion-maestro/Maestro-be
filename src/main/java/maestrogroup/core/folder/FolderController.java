cc05bf1537856b33367401f67f14481229bf5d47
jeongseungchan <jschan0911@naver.com>
2023-01-02 00:11:08 +0900

    feat: folder 관련 swagger 설명 추가

--- src/main/java/maestrogroup/core/folder/FolderController.java
+++ src/main/java/maestrogroup/core/folder/FolderController.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.folder;
 
+import io.swagger.v3.oas.annotations.Operation;
 import maestrogroup.core.ExceptionHandler.BaseException;
 import maestrogroup.core.ExceptionHandler.BaseResponse;
 import maestrogroup.core.ExceptionHandler.BaseResponseStatus;
@@ -40,6 +41,7 @@ public class FolderController {
     // folderIdx, folderImgUrl, folderName, teamIdx
     @ResponseBody
     @PostMapping("/create_folder/{teamIdx}")
+    @Operation(summary = "폴더 생성", description = "특정 팀의 폴더를 생성합니다.")
     public BaseResponse createFolder(@PathVariable("teamIdx") int teamIdx, @RequestBody PostFolderReq postFolderReq) {
         try {
             folderService.createFolder(postFolderReq, teamIdx);
@@ -52,6 +54,7 @@ public class FolderController {
     // 특정 팀에 대한 폴더 리스트 조회
     @ResponseBody
     @GetMapping("/get_all_folder/{teamIdx}")
+    @Operation(summary = "특정 팀의 폴더 목록 조회")
     public BaseResponse<List<Folder>> GetAllFolder(@PathVariable("teamIdx") int teamIdx) throws BaseException{
         try {
             List<Folder> folderList = folderProvider.GetAllFolder(teamIdx);
@@ -68,6 +71,7 @@ public class FolderController {
     // 해당 폴더에 저장되어있던 값들을 수정란에 띄어주는 것으로 가정
     @ResponseBody
     @PatchMapping("/patchFolder/{folderIdx}/{teamIdx}")
+    @Operation(summary = "폴더 정보 수정", description = "폴더 이름을 수정합니다.")
     public BaseResponse modifyFolder(@PathVariable("folderIdx") int folderIdx, @PathVariable("teamIdx") int teamIdx, @RequestBody ModifyFolderReq modifyFolderReq) {
         try {
             folderService.modifyFolder(folderIdx, teamIdx, modifyFolderReq);
@@ -80,6 +84,7 @@ public class FolderController {
     // 폴더를 삭제할떄 폴더안에 담겨있는 음악 객체들도 모두 삭제되도록 API 추가 개발 처리요망
     @ResponseBody
     @DeleteMapping("/deleteFolder/{folderIdx}")
+    @Operation(summary = "폴더 삭제")
     public BaseResponse deleteFolder(@PathVariable("folderIdx") int folderIdx) {
         try {
             folderService.deleteFolder(folderIdx);
@@ -91,6 +96,7 @@ public class FolderController {
 
     @ResponseBody
     @PatchMapping("changeImportantOfFolder/{folderIdx}")
+    @Operation(summary = "폴더 별 표시 기능", description = "폴더의 중요도를 나타내는 별 표시 기능입니다. important가 0이라면 1이 되고, 1이라면 0이 되도록 구현했습니다.")
     public BaseResponse changeImportantOfFolder(@PathVariable("folderIdx") int folderIdx) {
         try {
             folderService.changeImportantOfFolder(folderIdx);
