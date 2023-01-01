8698dba0ce63d4a4904d1e53e9086e17d8cb9001
msung99 <msung6924@naver.com>
2023-01-01 21:00:29 +0900

    feat: Swagger User 관련 기본 작업

--- src/main/java/maestrogroup/core/user/UserController.java
+++ src/main/java/maestrogroup/core/user/UserController.java
@@ -1,6 +1,8 @@
 package maestrogroup.core.user;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import maestrogroup.core.ExceptionHandler.BaseException;
 import maestrogroup.core.ExceptionHandler.BaseResponse;
 import maestrogroup.core.ExceptionHandler.BaseResponseStatus;
@@ -16,8 +18,10 @@ import javax.validation.Valid;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
+
 @RequestMapping("/user")
 @RestController
+@Api(value = "User", tags = "User 관련 API")
 public class UserController {
     @Autowired
     private final UserService userService;
@@ -47,6 +51,7 @@ public class UserController {
     // 회원가입
     @ResponseBody
     @PostMapping("/signUp")
+    @Operation(summary = "회원가입", description = "이메일, 비밀번호, 2차 비밀번호, 닉네임을 입력받는다.")
     public BaseResponse<SignUpUserRes> createUser(@RequestBody SignUpUserReq signUpUserReq) {
         try {
             SignUpUserRes signUpUserRes = userService.createUser(signUpUserReq);
@@ -59,6 +64,7 @@ public class UserController {
     // 로그인
     @ResponseBody
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "이메일, 비밀번호를 입력받고 로그인에 성공하면 accessToken, refreshToken을 발급받는다. 로컬 스토리에 저장할것!")
     public  BaseResponse<LoginUserRes>loginUser(@RequestBody LoginUserReq loginUserReq){
         try {
             LoginUserRes loginUserRes = userProvider.loginUser(loginUserReq);
@@ -69,6 +75,7 @@ public class UserController {
     }
 
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃", description = "로그아웃 호출시 클라이언트는 local storage 에 저장하고 있던 access, refresh token을 모두 삭제해야한다.")
     public BaseResponse<LoginUserRes>logoutUser(){
         try{
             userProvider.logoutUser();
@@ -81,6 +88,7 @@ public class UserController {
     // 회원정보 수정
     @ResponseBody
     @PatchMapping("/modifyUser")
+    @Operation(summary = "회원정보(프로필) 수정", description = "jwt 를 HttpHeader에 넘겨주시고, 이메일, 닉네임, 비밀번호 필드 Http Body에 실어서 모두 서버에 Request 로 넘겨주세요.")
     public BaseResponse modifyUserInfo(@RequestBody ModifyUserInfoReq modifyUserInfoReq){
         try {
             int userIdxByJwt = jwtService.getUserIdx(); // 클라이언트로 부터 넘겨받은 JWT 토큰으로부터 userIdx 값 추출
@@ -95,6 +103,7 @@ public class UserController {
     // => 삭제시킬 것들 : User, Mapping
     @ResponseBody
     @DeleteMapping("/deleteUser")
+    @Operation(summary = "계정 삭제")
     public BaseResponse deleteUser() {
         try {
             int userIdx = jwtService.getUserIdx();
