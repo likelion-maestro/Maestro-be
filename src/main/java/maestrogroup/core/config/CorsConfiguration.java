4f25d9acd65c64bfbdd34fb8fe25913aff1a3917
msung99 <msung6924@naver.com>
2023-01-01 20:32:40 +0900

    feat: Swagger 의존성 주입

--- src/main/java/maestrogroup/core/config/CorsConfiguration.java
+++ src/main/java/maestrogroup/core/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package maestrogroup.core;
+package maestrogroup.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
