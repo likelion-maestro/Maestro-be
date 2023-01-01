a35391426c134aa3a822440ec497aa521d764530
jeongseungchan <jschan0911@naver.com>
2023-01-02 00:35:48 +0900

    feat: security 관련 swagger Request & Response 추가

--- src/main/java/maestrogroup/core/Security/JWTtoken/AccessToken.java
+++ src/main/java/maestrogroup/core/Security/JWTtoken/AccessToken.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.Security.JWTtoken;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccessToken {
+    @ApiModelProperty(example = "eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWR4IjoxMSwiaWF0IjoxNjcyNTg3MjgxLCJleHAiOjE2NzI1ODkwODF9.0zPVjNqR3B9vlCvYM6h3fdMduJoh6gak5mowOv84-KU")
     String accessToken;
 }
