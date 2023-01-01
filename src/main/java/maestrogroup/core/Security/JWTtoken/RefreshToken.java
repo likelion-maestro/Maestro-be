1ec1465bba46980f87148cc6da6a094219bd08fb
jeongseungchan <jschan0911@naver.com>
2023-01-02 00:35:48 +0900

    feat: security 관련 swagger Request & Response 추가

--- src/main/java/maestrogroup/core/Security/JWTtoken/RefreshToken.java
+++ src/main/java/maestrogroup/core/Security/JWTtoken/RefreshToken.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.Security.JWTtoken;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RefreshToken{
+    @ApiModelProperty(example = "eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWR4IjoxMSwiaWF0IjoxNjcyNTg3MjgxLCJleHAiOjE2NzMxOTIwODF9.oCGljA9e36qJoDS9jsfO3nLijNYoxvHM3ib01UF8-68")
     String refreshToken;
 }
--- src/main/java/maestrogroup/core/Security/JWTtoken/Token.java
+++ src/main/java/maestrogroup/core/Security/JWTtoken/Token.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.Security.JWTtoken;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Token {
+    @ApiModelProperty(example = "eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWR4IjoxMSwiaWF0IjoxNjcyNTg3MjgxLCJleHAiOjE2NzMxOTIwODF9.oCGljA9e36qJoDS9jsfO3nLijNYoxvHM3ib01UF8-68")
     private String refreshToken;
+
+    @ApiModelProperty(example = "eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VySWR4IjoxMSwiaWF0IjoxNjcyNTg3MjgxLCJleHAiOjE2NzI1ODkwODF9.0zPVjNqR3B9vlCvYM6h3fdMduJoh6gak5mowOv84-KU")
     private String accessToken;
 }
