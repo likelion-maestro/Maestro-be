25b81d73127c80059e2ba74d96283a05db3c5bde
jeongseungchan <jschan0911@naver.com>
2023-01-02 00:44:16 +0900

    feat: mapping 관련 swagger Request & Response 추가

--- src/main/java/maestrogroup/core/mapping/model/GetTeamAndImportantRes.java
+++ src/main/java/maestrogroup/core/mapping/model/GetTeamAndImportantRes.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.mapping.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetTeamAndImportantRes {
+    @ApiModelProperty(example = "4")
     private int teamIdx;
+
+    @ApiModelProperty(example = "멋쟁이 사자처럼 밴드")
     private String teamName;
+
+    @ApiModelProperty(example = "7")
     private int count;
+
+    @ApiModelProperty(example = "0")
     private int important;
 }
--- src/main/java/maestrogroup/core/mapping/model/Mapping.java
+++ src/main/java/maestrogroup/core/mapping/model/Mapping.java
@@ -1,5 +1,6 @@
 package maestrogroup.core.mapping.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Mapping {
+    @ApiModelProperty(example = "1")
     int mappingIdx;
+
+    @ApiModelProperty(example = "0")
     int important;
+
+    @ApiModelProperty(example = "1")
     int teamIdx;
+
+    @ApiModelProperty(example = "4")
     int userIdx;
 }
